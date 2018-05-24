5a96fa92de42d009031ef4bee231a1f47be78ef9
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-24 13:50:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const store = {
 let driverId = 0;
 class Driver {
   constructor(name){
-    this.id   = ++driverId;
+    this.id = ++driverId;
     this.name = name;
     store.drivers.push(this)
   }
@@ -18,10 +18,19 @@ class Driver {
 let passengerId = 0;
 class Passenger {
   constructor(name){
-    this.id   = ++passengerId;
+    this.id = ++passengerId;
     this.name = name;
     store.passengers.push(this)
   }
   trips(){}
   drivers(){}
+}
+
+let tripId = 0;
+class Trip {
+  constructor(driver, passenger){
+    this.id = ++tripId;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+  }
 }
