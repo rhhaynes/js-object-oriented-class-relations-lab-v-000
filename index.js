e581cf644d06b21d1ed316eb6ee8704a67f7fbbf
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-24 13:53:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ class Driver {
     store.drivers.push(this);
   }
   trips(){}
-  passengers(){
-    return store.filter(d => this.id === d.id);
-  }
+  passengers = store.filter(d => this.id === d.id);
 }
 
 let passengerId = 0;
