310360eeec0fd0f13f00dc25abb88701ebc69bf7
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-24 13:52:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ class Driver {
   constructor(name){
     this.id = ++driverId;
     this.name = name;
-    store.drivers.push(this)
+    store.drivers.push(this);
   }
   trips(){}
-  passengers(){store.filter(d => this.id === d.id)}
+  passengers(){
+    return store.filter(d => this.id === d.id)}
 }
 
 let passengerId = 0;
@@ -20,7 +21,7 @@ class Passenger {
   constructor(name){
     this.id = ++passengerId;
     this.name = name;
-    store.passengers.push(this)
+    store.passengers.push(this);
   }
   trips(){}
   drivers(){}
